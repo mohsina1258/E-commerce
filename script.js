4325142ddd72488ea93247746923216292930711
Mohsin Alvi <alvimohsin900@gmail.com>
2023-08-31 15:54:17 +0500

    fully responsive e-commerce website

--- script.js
+++
@@ -1,8 +0,0 @@
-// const bar = document.getElementsByClassName('bar');
-// const nav = document.getElementsByClassName('navbar');
-// if (bar) {
-//     bar.addEventListener('click', () => {
-//         nav.classList.add('active');
-
-//     })
-// }
